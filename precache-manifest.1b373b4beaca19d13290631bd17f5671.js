fe0e9c59dfc996e581ed1afe10ac88456afdbd36
Ajay Rajamani <ajayrajamani@gmail.com>
2020-09-11 23:24:38 -0700

    Updates

---
+++ precache-manifest.1b373b4beaca19d13290631bd17f5671.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200ab84a6a76c8aa9fa239888b18e5b3",
+    "url": "/Simons-Apprentice/index.html"
+  },
+  {
+    "revision": "1e6dcd26114d4033ae0b",
+    "url": "/Simons-Apprentice/static/css/main.a3cae982.chunk.css"
+  },
+  {
+    "revision": "5345ce95ac816d192a6d",
+    "url": "/Simons-Apprentice/static/js/2.652e9e28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simons-Apprentice/static/js/2.652e9e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6dcd26114d4033ae0b",
+    "url": "/Simons-Apprentice/static/js/main.cd524fbb.chunk.js"
+  },
+  {
+    "revision": "922ceb6c21db0893e10e",
+    "url": "/Simons-Apprentice/static/js/runtime-main.d7c8acf7.js"
+  },
+  {
+    "revision": "5b003b64f89933f69b4d10e8e6fe9929",
+    "url": "/Simons-Apprentice/static/media/IMMORTAL.5b003b64.ttf"
+  },
+  {
+    "revision": "799b2ce9d542fa31757f933ddf2c5a5b",
+    "url": "/Simons-Apprentice/static/media/background-mobile.799b2ce9.jpg"
+  },
+  {
+    "revision": "fd1f041c2dcaa7ce74ed86280b74a9c7",
+    "url": "/Simons-Apprentice/static/media/blueOrb.fd1f041c.png"
+  },
+  {
+    "revision": "b6c13a1502b44afc859decbc134128da",
+    "url": "/Simons-Apprentice/static/media/blueSprite.b6c13a15.gif"
+  },
+  {
+    "revision": "b9ff05f9555538a1d6943fb897abe50c",
+    "url": "/Simons-Apprentice/static/media/circle.b9ff05f9.jpg"
+  },
+  {
+    "revision": "0ecc18da5bb3963a3e95e232d1b6a68e",
+    "url": "/Simons-Apprentice/static/media/greenOrb.0ecc18da.png"
+  },
+  {
+    "revision": "ea067f665b448350b8c5f25b56edaf1e",
+    "url": "/Simons-Apprentice/static/media/greenSprite.ea067f66.gif"
+  },
+  {
+    "revision": "50e75402fe4e017a0e3818a86adecf77",
+    "url": "/Simons-Apprentice/static/media/redOrb.50e75402.png"
+  },
+  {
+    "revision": "829b5c15f96f417248aefe54fe5c5a62",
+    "url": "/Simons-Apprentice/static/media/redSprite.829b5c15.gif"
+  },
+  {
+    "revision": "ea689330e268dd5542775c085a72d879",
+    "url": "/Simons-Apprentice/static/media/yellowOrb.ea689330.png"
+  },
+  {
+    "revision": "49a3dbf3727fb262138af6cd6ade1173",
+    "url": "/Simons-Apprentice/static/media/yellowSprite.49a3dbf3.gif"
+  }
+]);
