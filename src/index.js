6f4ff09df7589076547a62ce9641dda4024d9522
Ajay Rajamani <ajayrajamani@gmail.com>
2020-09-12 21:43:43 -0700

    Seed high scores

--- src/index.js
+++ src/index.js
@@ -8,7 +8,18 @@ import App from './App.jsx';
 if(!JSON.parse(localStorage.data)){
   localStorage.data = JSON.stringify({
     scores:[
-      {name: "Harry Dresden", rank:30 }
+      { name: "Yendor", rank: 34 },
+      { name: "Elon", rank: 32 },
+      { name: "Harry Dresden", rank:30 },
+      { name: "Glinda", rank: 28 },
+      { name: "Gandalf the White", rank: 26 },
+      { name: "Gandalf the Gray", rank: 24 },
+      { name: "Hermione", rank: 20 },
+      { name: "Oz", rank: 18 },
+      { name: "Steve", rank: 10 },
+      { name: "Alfred", rank: 8 },
+      { name: "Morty", rank: 4 }
+      
     ]
   })
 }
